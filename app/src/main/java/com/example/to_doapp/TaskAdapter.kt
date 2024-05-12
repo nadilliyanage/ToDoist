15499a649cee6ce1797747a08866d1706818333a
Nadil Senruka <it22144430@my.sliit.lk>
2024-05-12 10:34:52 +0530

    delete done

--- app/src/main/java/com/example/to_doapp/TaskAdapter.kt
+++ app/src/main/java/com/example/to_doapp/TaskAdapter.kt
@@ -7,15 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class TaskAdapter(private var tasks: List<Task>,  context: Context) :
     RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
 
+        private val  db: TaskDatabaseHelper = TaskDatabaseHelper(context)
+
     class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
         val contentTextView: TextView = itemView.findViewById(R.id.contentTextView)
         val updateButton: ImageView = itemView.findViewById(R.id.updateButton)
+        val deleteButton: ImageView = itemView.findViewById(R.id.deleteButton)
     }
 
     // Setup the item layout view
@@ -41,6 +45,12 @@ class TaskAdapter(private var tasks: List<Task>,  context: Context) :
                 putExtra("task_id", task.id)
             }
             holder.itemView.context.startActivity(intent)
+        }
+
+        holder.deleteButton.setOnClickListener {
+            db.deleteTask(task.id)
+            refreshData(db.getAllTasks())
+            Toast.makeText(holder.itemView.context, "Task Deleted", Toast.LENGTH_SHORT).show()
         }
     }
 
